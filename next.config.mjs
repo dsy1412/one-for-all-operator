772ab22dd47cf02a1c3fd1804346752ece00d028
dsy1412 <2271323478@qq.com>
2026-04-21 21:18:52 -0400

    fix: update basePath detection using VERCEL env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig = {
-  /* config options here */
+  basePath: isVercel ? "/eml" : "",
+  assetPrefix: isVercel ? "/eml" : "",
 };
 
 export default nextConfig;
